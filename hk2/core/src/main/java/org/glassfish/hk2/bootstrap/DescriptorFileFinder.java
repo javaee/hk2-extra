e790ae31d07a2e8cddcd32635fd230c566bc0ebc
Romain Grecourt <romain.grecourt@oracle.com>
2014-03-12 13:16:53 +0100

    move osgi-resource-locator to the top and remove hk2 directory

--- hk2/core/src/main/java/org/glassfish/hk2/bootstrap/DescriptorFileFinder.java
+++
@@ -1,9 +0,0 @@
-package org.glassfish.hk2.bootstrap;
-
-/**
- * @deprecated use {@link org.glassfish.hk2.api.DescriptorFileFinder} instead
- * @author jwells
- *
- */
-public interface DescriptorFileFinder extends org.glassfish.hk2.api.DescriptorFileFinder {
-}
--- hk2/examples/security-lockdown/runner/src/main/java/Dummy.java
+++
@@ -1,4 +0,0 @@
-/*  This is a dummmy class that forces Maven to create a source artifact for this module */
-/*  Source artifact is required to deploy to maven.java.net.  */
-public class Dummy {
-}
